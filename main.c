50e96478422f1dfdd6b8e0c9b3a42ddf528f9238
Paweł Murias <pawelmurias@gmail.com>
2011-09-03 13:03:54 +0200

    walls are placed correctly

--- main.c
+++ main.c
@@ -73,10 +73,10 @@ void draw_walls(TileMap * map, Camera * cam, Sprite ** tileset)
 
 			if (tileX >= 0 && tileY >= 0 && tileX < map->width && tileY < map->height) {
 				int tid = map->wall_tiles[(int)(map->width * tileY + tileX)];
-				if (tid) {
-					Sprite *tile = tileset[tid];
-					isozbatch_add_sprite(tile, tileX, tileY);
-				}
+				if (tid & 2) isozbatch_add_sprite(tileset[1], tileX, tileY);
+				if (tid & 4) isozbatch_add_sprite(tileset[2], tileX, tileY);
+				if (tid & 8) isozbatch_add_sprite(tileset[3], tileX, tileY);
+				if (tid & 16) isozbatch_add_sprite(tileset[4], tileX, tileY);
 			}
 		}
 }
--- map_gen.c
+++ map_gen.c
@@ -21,7 +21,7 @@ static void render(TileMap* map,char* filename) {
 	fprintf(f,"P1\n");
 	fprintf(f,"%d %d\n",map->width,map->height);
 	for (int y = 0; y < map->height; y++) {
-		for (int x = 0; x < map->height; x++) {
+		for (int x = 0; x < map->width; x++) {
 			fprintf(f,"%d ",map->tiles[map->width*y+x] != 0);
 		}
 		fprintf(f,"\n");
@@ -38,15 +38,31 @@ TileMap *overworld_gen(int w, int h)
 	map->height = h;
 	for (int i = 0; i < w * h; i++) {
 		map->tiles[i] = 1+rand_rand()%4;
-		int a = rand_rand()%5;
-		int b = rand_rand()%5;
-		map->wall_tiles[i] = (a==b ? a : 0);
+		map->wall_tiles[i] = 0;
+//		int a = rand_rand()%5;
+//		int b = rand_rand()%5;
+//		map->wall_tiles[i] = (a==b ? a : 0);
 	}
 
 	int holes = 300;
 	for (int i = 0; i < holes; i++) {
 		dig(map,rand_rand()%w,rand_rand()%h);
 	}
+
+	for (int y = 1; y < map->height-1; y++) {
+		for (int x = 1; x < map->width-1; x++) {
+		    if (map->tiles[y*map->width+x] == 0) {
+		    	if (map->tiles[y*map->width+x] == 0) {
+				if (map->tiles[(y+1)*map->width+x]) map->wall_tiles[y*map->width+x] |= 2;
+				if (map->tiles[(y)*map->width+x+1]) map->wall_tiles[y*map->width+x] |= 4;
+				if (map->tiles[(y-1)*map->width+x]) map->wall_tiles[y*map->width+x] |= 8;
+				if (map->tiles[(y)*map->width+x-1]) map->wall_tiles[y*map->width+x] |= 16;
+			}
+		    }
+				
+		}
+	}
+
 	render(map,"map.pbm");
 	return map;
 }
